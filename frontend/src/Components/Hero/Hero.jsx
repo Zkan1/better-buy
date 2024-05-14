c3c113288b03491684ae459df14dfd6c767bc949
Özkan Kuruoğlu <92784850+Zkan1@users.noreply.github.com>
2024-05-14 23:40:50 +0300

    Homepage'e Popular ve Offers kismi eklendi

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import './Hero.css'
-import hand_icon from '../Assets/hand_icon.png'
-import arrow_icon from '../Assets/arrow.png'
+import React from 'react';
+import './Hero.css';
+import hand_icon from '../Assets/hand_icon.png';
+import arrow_icon from '../Assets/arrow.png';
+import hero_image from '../Assets/hero_image.png';
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className="hero">
       <div className="hero-left">
-        <h2>Yeni Üyeler İçin </h2>
-      </div>
-      <div className="div hero right">
-        <div className="hand-hand-icon">
-            <p>Yeni</p>
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
             <img src={hand_icon} alt="" />
+          </div>
+          <p>collections</p>
+          <p>for everyone</p>
         </div>
-        <p>Koleksiyon</p>
-        <p>Herkes için</p>
-
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
       </div>
-      <div className="hero-latest-btn">
-        <div>En Yeni Parçalar</div>
-        <img src={arrow_icon} alt="" />
+      <div className="hero-right">
+        <img src={hero_image} alt="hero" />
       </div>
     </div>
   )
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from 'react'   //AnaSayfa
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
+      <Offers/>
     </div>
   )
 }
