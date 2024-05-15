693b89e04c46c2d43e290295161172afdf98dd6d
Özkan Kuruoğlu <92784850+Zkan1@users.noreply.github.com>
2024-05-15 18:04:04 +0300

    descriptionbox eklendi

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+ import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox' >
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, molestiae aliquam est a, asperiores ut quia magnam quo cumque, deleniti deserunt. Et facilis doloribus dignissimos atque suscipit aut necessitatibus, ea iusto itaque cum quas cumque consectetur placeat autem fugiat neque inventore quis dicta, consequatur alias laudantium optio. Non, labore repellat!</p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero doloremque possimus sapiente tempore dolore incidunt vel, harum minima fugit quisquam!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
