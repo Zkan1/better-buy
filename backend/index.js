1cfa1fb7324e568ceedbeae5ce9dbb9d751afbcd
Özkan Kuruoğlu <92784850+Zkan1@users.noreply.github.com>
2024-05-31 18:22:17 +0300

    ürün ekleme ve silme fonksiyonları eklendi

--- backend/index.js
+++ backend/index.js
@@ -44,8 +44,81 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     })
 })
 
+// Ürün oluşturma şeması 
+const Product = mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avilable:{
+        type:Boolean,
+        dafault:true,
+    },
+    
+})
 
+app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id ;
+    if(products.length>0)
+        {
+            let last_product_array = products.slice(-1);
+            let last_product = last_product_array[0];
+            id= last_product.id+1;
+        }else{
+            id=1;
+        }//id numarasını her yeni ürün eklendiğinde 1 artırmak için eğer eklenen ilk ürünse id 1 oluyor
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
 
+    });
+    console.log(product);
+    await product.save();
+    console.log("saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+//API ürün silme
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("remove");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
 app.listen(port,(error)=>{
     if (!error){
         console.log("Server Running on Port "+port)
