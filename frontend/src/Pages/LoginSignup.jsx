fd1a3b1223e3152d2807d448b6bc149644c39904
Özkan Kuruoğlu <92784850+Zkan1@users.noreply.github.com>
2024-06-01 13:18:04 +0300

    giris yap ve kayıt ol sayfasi düzenlendi

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/LoginSignup.css'
 const LoginSignup = () => {
+
+
+  const [state,setState]=useState("Login");
+  const[formData,setFromData]= useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler=(e)=>{
+    setFromData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const login = async()=>{
+    console.log("login çalışıyor",formData);
+  }
+  const signup = async()=>{
+    console.log("signup çalışıyor",formData);
+  }
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+         {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' />:<></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address' />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
         </div>
-        <button>Contiune</button>
-        <p className="loginsignup-login">Already have an account? <span>Login Here</span></p>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Contiune</button>
+        {state==="Sign Up"? <p className="loginsignup-login">Already have an account? <span onClick={()=>{setState("Login")}}>Login Here</span></p>:
+        <p className="loginsignup-login">Create an account <span onClick={()=>{setState("Sign Up")}}>Click Here</span></p>}
+       
+        
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
           <p>By Continuing, i agree to the terms of use & privacy policy</p>
